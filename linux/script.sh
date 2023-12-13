0c84ab6c79632aa0b6a8ef39447993700595bd3d
Gabriel Jeronimo <gabrieljeronimo-@hotmail.com>
2023-12-13 14:44:29 -0300

    Remove comments

--- linux/script.sh
+++ linux/script.sh
@@ -1,7 +1,6 @@
-#Usando o Caps Lock + WASD você terá seus direcionais CIMA, ESQUERDA, BAIXO, DIREITA.
-#Using Caps Lock + WASD you will have your UP, LEFT, DOWN, RIGHT directional.
 xmodmap -e "keycode 66 = Mode_switch" &&
 xmodmap -e "keysym h = h H Left" &&
 xmodmap -e "keysym j = j J Down" &&
 xmodmap -e "keysym k = k K Up" &&
 xmodmap -e "keysym l = l L Right"
+
